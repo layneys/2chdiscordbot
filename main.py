5dda79056241d8117b6bdefe7784561005918d31
layneys <coolan151103711@yandex.ru>
2022-12-09 00:19:10 +0300

    Resurrected dead folder

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests, discord, os
+from bs4 import BeautifulSoup
+from random import randint
+
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$pasta'):
+        random_num = randint(1, 520)
+
+        headers = {
+            "Accept": "*/*",
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0"
+        }
+
+        url = f"https://pastach.ru/p/{random_num}"
+
+        req = requests.get(url, headers=headers)
+        src = req.text
+
+        soup = BeautifulSoup(src, "lxml")
+
+        paste = soup.find(class_="paste-content")
+        await message.channel.send(f'{paste.txt}')
+
+client.run(os.getenv('ODk0MzEwMjM2MzEwMTEwMjcx.YVoJTw.-GmX7vKfhbgYL7Q9-EWv2sbgymc'))
